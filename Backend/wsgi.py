9a5dcac06f6f0db0d6ece7fa5706e055e30e17ec
chiraglokhande04 <chiraglokhande14@gmail.com>
2024-10-29 01:00:45 +0530

    twelfth commit

--- Backend/wsgi.py
+++ Backend/wsgi.py
@@ -1,6 +1,5 @@
 from app import app
-import os
+
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))  # default to 5000 if PORT is not set
-    app.run(port=port, debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
